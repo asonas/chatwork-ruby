0e3aae12a165cf644169605b96ed3b1921262d99
asonas <hzw1258@gmail.com>
2013-11-28 12:49:53 +0900

    Add entity

---
+++ lib/chatwork/entity.rb
@@ -0,0 +1,29 @@
+module ChatWork
+  class Entity
+    class << self
+      include Operations
+      def convert(hash)
+        # not implement
+        #converter = ResponseConverter.new
+        #converter.convert(hash)
+        return hash
+      end
+    end
+
+    attr_reader :attributes
+
+    def initialize(attributes)
+      @attributes = attributes
+    end
+
+    private
+
+    # not implement
+    def update_attributes(attributes)
+      raise "unexpected object" if attributes['object'] != @attributes['object']
+      new_object = ResponseConverter.new.convert(attributes)
+      @attributes = new_object.attributes
+      self
+    end
+  end
+end
