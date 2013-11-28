637e7cbf4b3cc58e1690554b953e6c2f7764c8b0
asonas <hzw1258@gmail.com>
2013-11-28 12:17:15 +0900

    Add api endpoint

--- lib/chatwork.rb
+++ lib/chatwork.rb
@@ -1,5 +1,33 @@
 require "chatwork/version"
 
-module Chatwork
-  # Your code goes here...
+module ChatWork
+  autoload(:Client, 'chatwork/client')
+
+  @api_base = 'https://api.chatwork.com/'
+  @api_version = '/v1'
+  @api_key = nil
+
+  class << self
+    def client
+      @client ||= Cluent.new(@api_key, @api_base, @api_version)
+    end
+
+    def api_base=(new_value)
+      @api_base = new_value
+      @client = nil
+    end
+
+    def api_key=(new_value)
+      @api_key = new_value
+      @client = nil
+    end
+
+    def api_base
+      @api_base
+    end
+
+    def api_key
+      @api_key
+    end
+  end
 end
