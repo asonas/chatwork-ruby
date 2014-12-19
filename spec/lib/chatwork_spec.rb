38718481d333289dc3ab6742dafbac64463e09eb
asonas <hzw1258@gmail.com>
2014-12-19 19:39:15 +0900

    Upgrade rspec and execute transpec command

--- spec/lib/chatwork_spec.rb
+++ spec/lib/chatwork_spec.rb
@@ -2,12 +2,31 @@ require 'spec_helper'
 require 'chatwork'
 
 describe ChatWork do
-  its(:client) { should be_a(ChatWork::Client) }
-  its(:api_base) { should eq('https://api.chatwork.com/') }
-  describe '#api_base=' do
-    before { subject.api_base = 'https://test.example.com/' }
-    its(:api_base) { should eq('https://test.example.com/') }
-    its(:api_base) { should eq('https://test.example.com/') }
+  describe '#client' do
+    subject { super().client }
+    it { should be_a(ChatWork::Client) }
   end
-  its(:api_key) { should be_nil }
+
+  describe '#api_base' do
+    subject { super().api_base }
+    it { should eq('https://api.chatwork.com/') }
+  end
+  describe '#api_base=' do
+    before { ChatWork.api_base = 'https://test.example.com/' }
+
+    describe '#api_base' do
+      subject { super().api_base }
+      it { should eq('https://test.example.com/') }
+    end
+
+    describe '#api_base' do
+      subject { super().api_base }
+      it { should eq('https://test.example.com/') }
+    end
+  end
+
+  describe '#api_key' do
+    subject { super().api_key }
+    it { should be_nil }
+  end
 end
