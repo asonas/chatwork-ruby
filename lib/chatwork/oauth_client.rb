e857c1c2d73cca656458cdef0a792e9642c5058a
sue445 <sue445@sue445.net>
2018-01-07 00:12:39 +0900

    :cop: rubocop --auto-correct --only Performance/StringReplacement

--- lib/chatwork/oauth_client.rb
+++ lib/chatwork/oauth_client.rb
@@ -3,7 +3,7 @@ require 'base64'
 module ChatWork
   class OAuthClient < BaseClient
     def initialize(client_id, client_secret, oauth_api_base)
-      signature = Base64.encode64("#{client_id}:#{client_secret}").gsub("\n", "")
+      signature = Base64.encode64("#{client_id}:#{client_secret}").delete("\n")
       super(oauth_api_base, "", { 'Authorization' => "Basic #{signature}" })
     end
   end
