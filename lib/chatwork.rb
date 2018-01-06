6fc814a2d00208e9def482e770f8e526dfb20e90
sue445 <sue445@sue445.net>
2018-01-07 00:47:06 +0900

    Cosmetic change :lipstick:

--- lib/chatwork.rb
+++ lib/chatwork.rb
@@ -1,22 +1,22 @@
 require "chatwork/version"
 
 module ChatWork
-  autoload(:BaseClient, "chatwork/base_client")
-  autoload(:Client, "chatwork/client")
-  autoload(:OAuthClient, "chatwork/oauth_client")
-  autoload(:Token, "chatwork/token")
-  autoload(:Operations, "chatwork/operations")
-  autoload(:ChatWorkError, "chatwork/chatwork_error")
-  autoload(:APIConnectionError, "chatwork/chatwork_error")
-  autoload(:APIError, "chatwork/chatwork_error")
-  autoload(:Room, "chatwork/room")
-  autoload(:Entity, "chatwork/entity")
-  autoload(:Message, "chatwork/message")
-  autoload(:Me, "chatwork/me")
-  autoload(:MyTask, "chatwork/my_task")
-  autoload(:Task, "chatwork/task")
-  autoload(:Member, "chatwork/member")
-  autoload(:Contacts, "chatwork/contacts")
+  autoload :BaseClient,         "chatwork/base_client"
+  autoload :APIConnectionError, "chatwork/chatwork_error"
+  autoload :APIError,           "chatwork/chatwork_error"
+  autoload :ChatWorkError,      "chatwork/chatwork_error"
+  autoload :Client,             "chatwork/client"
+  autoload :Contacts,           "chatwork/contacts"
+  autoload :Entity,             "chatwork/entity"
+  autoload :Me,                 "chatwork/me"
+  autoload :Member,             "chatwork/member"
+  autoload :Message,            "chatwork/message"
+  autoload :MyTask,             "chatwork/my_task"
+  autoload :OAuthClient,        "chatwork/oauth_client"
+  autoload :Operations,         "chatwork/operations"
+  autoload :Room,               "chatwork/room"
+  autoload :Task,               "chatwork/task"
+  autoload :Token,              "chatwork/token"
 
   @api_base = "https://api.chatwork.com/"
   @oauth_api_base = "https://oauth.chatwork.com/"
