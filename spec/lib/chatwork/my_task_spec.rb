ffdab257905a98771b1602f0b76e8696b9d10f46
sue445 <sue445@sue445.net>
2018-01-07 09:51:30 +0900

    Add test for ChatWork::MyTask.get

---
+++ spec/lib/chatwork/my_task_spec.rb
@@ -0,0 +1,19 @@
+describe ChatWork::MyTask do
+  describe ".get", type: :api do
+    subject do
+      ChatWork::MyTask.get(
+        assigned_by_account_id: assigned_by_account_id,
+        status:                 status,
+      )
+    end
+
+    let(:assigned_by_account_id) { 78 }
+    let(:status)                 { "done" }
+
+    before do
+      stub_chatwork_request(:get, "/my/tasks", "/my/tasks")
+    end
+
+    it_behaves_like :a_chatwork_api, :get, "/my/tasks"
+  end
+end
