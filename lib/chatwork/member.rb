f07cc8e896f3e95ea991a6a5f5b86ecae72baddb
sue445 <sue445@sue445.net>
2018-01-10 00:00:09 +0900

    Impl ChatWork::Member.update_all

--- lib/chatwork/member.rb
+++ lib/chatwork/member.rb
@@ -26,5 +26,33 @@ module ChatWork
     def self.get(room_id:)
       _get("/rooms/#{room_id}/members")
     end
+
+    # Change associated members of group chat at once
+    #
+    # @see http://developer.chatwork.com/ja/endpoint_rooms.html#PUT-rooms-room_id-members
+    #
+    # @param room_id [Integer]
+    # @param members_admin_ids [Array<Integer>, String] List of user IDs who will be given administrator permission for the group chat.
+    #                                                   At least one user must be specified as an administrator.
+    # @param members_member_ids [Array<Integer>, String] List of user IDs who will be given member permission for the group chat.
+    # @param members_readonly_ids [Array<Integer>, String] List of user IDs who will be given read-only permission for the group chat.
+    #
+    # @return [Hash]
+    #
+    # @example response format
+    # {
+    #   "admin": [123, 542, 1001],
+    #   "member": [10, 103],
+    #   "readonly": [6, 11]
+    # }
+    def self.update_all(room_id:, members_admin_ids:, members_member_ids: nil, members_readonly_ids: nil)
+      params = {
+        members_admin_ids: Array(members_admin_ids).join(","),
+      }
+      params[:members_member_ids] = Array(members_member_ids).join(",") if members_member_ids
+      params[:members_readonly_ids] = Array(members_readonly_ids).join(",") if members_readonly_ids
+
+      _put("/rooms/#{room_id}/members", params)
+    end
   end
 end
--- spec/lib/chatwork/member_spec.rb
+++ spec/lib/chatwork/member_spec.rb
@@ -10,4 +10,37 @@ describe ChatWork::Member do
 
     it_behaves_like :a_chatwork_api, :get, "/rooms/{room_id}/members"
   end
+
+  describe ".update_all", type: :api do
+    subject do
+      ChatWork::Member.update_all(
+        room_id:              room_id,
+        members_admin_ids:    members_admin_ids,
+        members_member_ids:   members_member_ids,
+        members_readonly_ids: members_readonly_ids,
+      )
+    end
+
+    let(:room_id) { 123 }
+
+    before do
+      stub_chatwork_request(:put, "/rooms/#{room_id}/members", "/rooms/{room_id}/members")
+    end
+
+    context "with String" do
+      let(:members_admin_ids)    { "123,542,1001" }
+      let(:members_member_ids)   { "21,344" }
+      let(:members_readonly_ids) { "15,103" }
+
+      it_behaves_like :a_chatwork_api, :put, "/rooms/{room_id}/members"
+    end
+
+    context "with Array" do
+      let(:members_admin_ids)    { [123, 542, 1001] }
+      let(:members_member_ids)   { [21, 344] }
+      let(:members_readonly_ids) { [15, 103] }
+
+      it_behaves_like :a_chatwork_api, :put, "/rooms/{room_id}/members"
+    end
+  end
 end
