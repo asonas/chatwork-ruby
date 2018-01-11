912f615c5fb1cbad9329d8fe9150b0c6696cf895
sue445 <sue445@sue445.net>
2018-01-11 23:28:13 +0900

    Impl ChatWork::File.find

--- lib/chatwork/file.rb
+++ lib/chatwork/file.rb
@@ -29,5 +29,33 @@ module ChatWork
     def self.get(room_id:, account_id:)
       _get("/rooms/#{room_id}/files", account_id: account_id)
     end
+
+    # Get information about the specified file
+    #
+    # @see http://developer.chatwork.com/ja/endpoint_rooms.html#GET-rooms-room_id-files-file_id
+    #
+    # @param room_id [Integer]
+    # @param file_id [Integer]
+    # @param create_download_url [Boolean] whether or not to create a download link.
+    #                                      If set to true, download like will be created for 30 seconds
+    #
+    # @return [Array<Hash>]
+    #
+    # @example response format
+    #   {
+    #     "file_id":3,
+    #     "account": {
+    #       "account_id":123,
+    #       "name":"Bob",
+    #       "avatar_image_url": "https://example.com/ico_avatar.png"
+    #     },
+    #     "message_id": "22",
+    #     "filename": "README.md",
+    #     "filesize": 2232,
+    #     "upload_time": 1384414750
+    #   }
+    def self.find(room_id:, file_id:, create_download_url: nil)
+      _get("/rooms/#{room_id}/files/#{file_id}", create_download_url: boolean_to_integer(create_download_url))
+    end
   end
 end
--- spec/lib/chatwork/file_spec.rb
+++ spec/lib/chatwork/file_spec.rb
@@ -11,4 +11,27 @@ describe ChatWork::File do
 
     it_behaves_like :a_chatwork_api, :get, "/rooms/{room_id}/files"
   end
+
+  describe ".find", type: :api do
+    subject { ChatWork::File.find(room_id: room_id, file_id: file_id, create_download_url: create_download_url) }
+
+    before do
+      stub_chatwork_request(:get, "/rooms/#{room_id}/files/#{file_id}", "/rooms/{room_id}/files/{file_id}")
+    end
+
+    let(:room_id) { 123 }
+    let(:file_id) { 101 }
+
+    context "when force is Integer" do
+      let(:create_download_url) { 1 }
+
+      it_behaves_like :a_chatwork_api, :get, "/rooms/{room_id}/files/{file_id}"
+    end
+
+    context "when force is boolean" do
+      let(:create_download_url) { true }
+
+      it_behaves_like :a_chatwork_api, :get, "/rooms/{room_id}/files/{file_id}"
+    end
+  end
 end
