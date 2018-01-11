7a06884b1ebc558c6394cd7de7211ea1e53b9ca5
sue445 <sue445@sue445.net>
2018-01-11 23:59:31 +0900

    Impl ChatWork::IncomingRequest.update

--- lib/chatwork/incoming_request.rb
+++ lib/chatwork/incoming_request.rb
@@ -27,5 +27,28 @@ module ChatWork
     def self.get
       _get("/incoming_requests")
     end
+
+    # You can approve a contact approval request you received
+    #
+    # @see http://developer.chatwork.com/ja/endpoint_incoming_requests.html#PUT-incoming_requests-request_id
+    #
+    # @param request_id [Integer]
+    #
+    # @return [Hash]
+    #
+    # @example response format
+    #   {
+    #     "account_id": 363,
+    #     "room_id": 1234,
+    #     "name": "John Smith",
+    #     "chatwork_id": "tarochatworkid",
+    #     "organization_id": 101,
+    #     "organization_name": "Hello Company",
+    #     "department": "Marketing",
+    #     "avatar_image_url": "https://example.com/abc.png"
+    #   }
+    def self.update(request_id:)
+      _put("/incoming_requests/#{request_id}")
+    end
   end
 end
--- spec/lib/chatwork/incoming_request_spec.rb
+++ spec/lib/chatwork/incoming_request_spec.rb
@@ -8,4 +8,16 @@ describe ChatWork::IncomingRequest do
 
     it_behaves_like :a_chatwork_api, :get, "/incoming_requests"
   end
+
+  describe ".update", type: :api do
+    subject { ChatWork::IncomingRequest.update(request_id: request_id) }
+
+    let(:request_id) { 123 }
+
+    before do
+      stub_chatwork_request(:put, "/incoming_requests/#{request_id}", "/incoming_requests/{request_id}")
+    end
+
+    it_behaves_like :a_chatwork_api, :put, "/incoming_requests/{request_id}"
+  end
 end
