a7af68cf64d61d4fc8f4b51da45ebab964c4c312
sue445 <sue445@sue445.net>
2018-02-04 23:24:40 +0900

    Impl InvitationLink.create

--- lib/chatwork/invitation_link.rb
+++ lib/chatwork/invitation_link.rb
@@ -21,5 +21,33 @@ module ChatWork
     def self.get(room_id:)
       _get("/rooms/#{room_id}/link")
     end
+
+    # Create invitation link
+    #
+    # @see http://developer.chatwork.com/ja/endpoint_rooms.html#POST-rooms-room_id-link
+    # @see http://download.chatwork.com/ChatWork_API_Documentation.pdf
+    #
+    # @param room_id [Integer]
+    # @param code [String] link path (default. random string)
+    # @param description [String] description of link page
+    # @param need_acceptance [Boolean] Approval necessity. Whether participation requires administrator approval.
+    #
+    # @return [Hashie::Mash]
+    #
+    # @example response format
+    #   {
+    #     "public": true,
+    #     "url": "https://example.chatwork.com/g/unique-link-name",
+    #     "need_acceptance": true,
+    #     "description": "This is a public room for topic A."
+    #   }
+    def self.create(room_id:, code: nil, description: nil, need_acceptance: nil)
+      params = {
+        code:            code,
+        description:     description,
+        need_acceptance: boolean_to_integer(need_acceptance),
+      }
+      _post("/rooms/#{room_id}/link", params)
+    end
   end
 end
--- spec/lib/chatwork/invitation_link_spec.rb
+++ spec/lib/chatwork/invitation_link_spec.rb
@@ -10,4 +10,26 @@ describe ChatWork::InvitationLink do
 
     it_behaves_like :a_chatwork_api, :get, "/rooms/{room_id}/link"
   end
+
+  describe ".create", type: :api do
+    subject do
+      ChatWork::InvitationLink.create(
+        room_id:         room_id,
+        code:            code,
+        description:     description,
+        need_acceptance: need_acceptance,
+      )
+    end
+
+    let(:room_id)         { 123 }
+    let(:code)            { "unique-link-name" }
+    let(:description)     { "This is a public room for topic A." }
+    let(:need_acceptance) { true }
+
+    before do
+      stub_chatwork_request(:post, "/rooms/#{room_id}/link", "/rooms/{room_id}/link")
+    end
+
+    it_behaves_like :a_chatwork_api, :post, "/rooms/{room_id}/link"
+  end
 end
