de805486fd1ee0c4a3f84be42bbe111c372d56ae
sue445 <sue445@sue445.net>
2018-02-19 00:05:55 +0900

    Tweak spec

--- spec/lib/chatwork/contacts_spec.rb
+++ spec/lib/chatwork/contacts_spec.rb
@@ -6,6 +6,6 @@ describe ChatWork::Contacts do
       stub_chatwork_request(:get, "/contacts")
     end
 
-    it_behaves_like :a_chatwork_api, :get, "contacts"
+    it_behaves_like :a_chatwork_api, :get, "/contacts"
   end
 end
