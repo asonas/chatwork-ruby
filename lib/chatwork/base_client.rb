9cf4cb61ffa07fc9867f6591161c89c58d1ad8e3
sue445 <sue445@sue445.net>
2020-01-12 09:44:17 +0900

    Support faraday 1.0

--- lib/chatwork/base_client.rb
+++ lib/chatwork/base_client.rb
@@ -36,9 +36,16 @@ module ChatWork
 
     Faraday::Connection::METHODS.each do |method|
       define_method(method) do |url, args = {}, &block|
+        faraday_errors =
+          if Gem::Version.create(Faraday::VERSION) >= Gem::Version.create("1.0.0")
+            [Faraday::ClientError, Faraday::ServerError]
+          else
+            [Faraday::Error::ClientError]
+          end
+
         begin
           response = @conn.__send__(method, @api_version + url, hash_compact(args))
-        rescue Faraday::Error::ClientError => e
+        rescue *faraday_errors => e
           raise ChatWork::APIConnectionError.faraday_error(e)
         end
         payload = handle_response(response)
